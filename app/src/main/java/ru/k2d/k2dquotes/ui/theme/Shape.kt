2195840029719a416e5d91e2ffc8b76012a726ab
K2D2021 <zxtreme@yandex.ru>
2023-02-21 23:13:28 +0300

    Initial commit

---
+++ app/src/main/java/ru/k2d/k2dquotes/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package ru.k2d.k2dquotes.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(4.dp),
+    medium = RoundedCornerShape(4.dp),
+    large = RoundedCornerShape(0.dp)
+)
