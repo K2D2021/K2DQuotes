8c9e5036267d5fe684d35fe7afd1b1808615a3ba
K2D2021 <zxtreme@yandex.ru>
2023-02-21 23:21:03 +0300

    Refatoring.

--- app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
+++ app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
@@ -3,12 +3,8 @@ package ru.k2d.k2dquotes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import ru.k2d.k2dquotes.ui.theme.K2DQuotesTheme
 
