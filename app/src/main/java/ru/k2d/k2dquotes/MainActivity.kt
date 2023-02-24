8d9a319b612eddcc7213cb4be7b9a9cbf4437d62
K2D2021 <zxtreme@yandex.ru>
2023-02-24 23:46:50 +0300

    Learn how use image card and take a shadow on it.

--- app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
+++ app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
@@ -3,31 +3,102 @@ package ru.k2d.k2dquotes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Outline
-import androidx.compose.ui.graphics.Outline.Rectangle
-import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import ru.k2d.k2dquotes.ui.theme.K2DQuotesTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            /*K2DQuotesTheme {
+            val painter = painterResource(id = R.drawable.screenshot)
+            val description = "Some cool man"
+            val title = "Some cool man"
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)) {
+                ImageCard(painter = painter, contentDescription = description, title = title)
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+        ) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f
+                    )
+                )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+
+                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+        }
+    }
+}
+/*
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}*/
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    /*K2DQuotesTheme {
+        Greeting("Android")
+    }*/
+}
+
+
+/*K2DQuotesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -36,77 +107,61 @@ class MainActivity : ComponentActivity() {
                     Greeting("Android")
                 }
             }*/
-            //Greeting(name = "K2D")
-            Row(
-                modifier = Modifier
+//Greeting(name = "K2D")
+/*Row(
+    modifier = Modifier
 //                    .width(300.dp)
-                    //.fillMaxSize()
+        //.fillMaxSize()
 //                    .fillMaxSize(0.1f)
-                    //.fillMaxHeight(0.9f)
+        //.fillMaxHeight(0.9f)
 //                    .requiredHeight(1600.dp)
-                    .background(Color.Yellow),
+        .background(Color.Yellow),
 
-                //horizontalArrangement = Arrangement.SpaceAround,
-                //verticalAlignment = Alignment.CenterVertically
-            ) {
+    //horizontalArrangement = Arrangement.SpaceAround,
+    //verticalAlignment = Alignment.CenterVertically
+) {
 
-                Row(
-                    modifier = Modifier
+    Row(
+        modifier = Modifier
 //                    .width(300.dp)
-                        .fillMaxWidth(0.5f)
+            .fillMaxWidth(0.5f)
 //                    .fillMaxSize(0.1f)
-                        .fillMaxHeight(0.9f)
-                        .border(10.dp, Color.Black, RoundedCornerShape(10.dp))
-                        .padding(20.dp)
-                        .border(10.dp, Color.Black, RoundedCornerShape(10.dp))
-                        //.height(1600.dp)
-                        .background(Color.Green)
-                        .scrollable(
-                            orientation = Orientation.Vertical,
-                            state = rememberScrollState()
+            .fillMaxHeight(0.9f)
+            .border(10.dp, Color.Black, RoundedCornerShape(10.dp))
+            .padding(20.dp)
+            .border(10.dp, Color.Black, RoundedCornerShape(10.dp))
+            //.height(1600.dp)
+            .background(Color.Green)
+            .scrollable(
+                orientation = Orientation.Vertical,
+                state = rememberScrollState()
 
-                        ),
+            ),
 
-                            horizontalArrangement = Arrangement . SpaceAround,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = "One")
-                    Text(text = "Two")
-                    Text(text = "Three", modifier = Modifier
-                        .offset((-30).dp, (-30).dp)
-                        .clickable { }
-                        //.absoluteOffset(30.dp,30.dp)
-                    )
-                }
-                /*Row(
-                    modifier = Modifier
+                horizontalArrangement = Arrangement . SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = "One")
+        Text(text = "Two")
+        Text(text = "Three", modifier = Modifier
+            .offset((-30).dp, (-30).dp)
+            .clickable { }
+            //.absoluteOffset(30.dp,30.dp)
+        )
+    }
+    /*Row(
+        modifier = Modifier
 //                    .width(300.dp)
-                        .fillMaxWidth()
-                        //.fillMaxWidth(0.5f)
+            .fillMaxWidth()
+            //.fillMaxWidth(0.5f)
 //                    .fillMaxSize(0.1f)
-                        .fillMaxHeight(0.9f)
-                        .background(Color.Red),
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(text = "One")
-                    Text(text = "Two")
-                    Text(text = "Three")
-                }*/
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    K2DQuotesTheme {
-        Greeting("Android")
-    }
-}
+            .fillMaxHeight(0.9f)
+            .background(Color.Red),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = "One")
+        Text(text = "Two")
+        Text(text = "Three")
+    }*/
+}*/
