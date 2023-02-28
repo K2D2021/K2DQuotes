0211a9e506e841eb27daabe9c5b14e822ff3ea82
K2D2021 <zxtreme@yandex.ru>
2023-02-28 23:35:56 +0300

    Refactoring.

--- app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
+++ app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
@@ -3,84 +3,14 @@ package ru.k2d.k2dquotes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(Color(0xFF000000)),
-                contentAlignment = Alignment.Center
 
-            ) {
-                Text(
-                    text = buildAnnotatedString {
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Green,
-                                fontSize = 30.sp,
-                                textDecoration = TextDecoration.None
-                            )
-                        ) {
-                            append("J")
-                        }
-                        append("etpack ")
-                        withStyle(
-                            style = SpanStyle(
-                                color = Color.Green,
-                                fontSize = 30.sp,
-                                textDecoration = TextDecoration.None
-                            )
-                        ) {
-                            append("C")
-                        }
-                        append("ompose")
-                    },
-                    color = Color.White,
-                    fontSize = 15.sp,
-                    fontFamily = FontFamily.Monospace,
-                    fontStyle = FontStyle.Italic,
-                    fontWeight = FontWeight.Thin,
-                    textAlign = TextAlign.Center,
-                    textDecoration = TextDecoration.LineThrough
-
-                )
-            }
         }
     }
 }
 
 
-/*
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}*/
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    /*K2DQuotesTheme {
-        Greeting("Android")
-    }*/
-}
