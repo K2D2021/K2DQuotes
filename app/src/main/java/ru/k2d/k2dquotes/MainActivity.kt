50ae686cd75386def261a33fe08964e1790b6bbf
K2D2021 <zxtreme@yandex.ru>
2023-03-01 00:40:55 +0300

    Make two different boxes. And top box change color of both boxes, but change self color only every second time.

--- app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
+++ app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
@@ -7,42 +7,86 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import kotlin.properties.Delegates
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
+    var switcher by Delegates.notNull<Int>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        switcher = 0
+
         setContent {
-            ColorBox(
-                Modifier.fillMaxSize()
-            )
+            Column(Modifier.fillMaxSize()) {
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
+                }
+                ColorBox(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                        .background(color.value),
+                    switcher
+                )
+                {
+                    color.value = it
+                    changeSwitcher()
+                }
+                Box(
+                    modifier = Modifier
+                        .background(color.value)
+                        .weight(1f)
+                        .fillMaxSize()
+                )
+            }
         }
+    }
+
+    private fun changeSwitcher() {
+        switcher = if (switcher == 0) 1 else 0
     }
 }
 
 @SuppressLint("UnrememberedMutableState")
 @Composable
-fun ColorBox(modifier: Modifier = Modifier) {
-    val color = remember {
-        mutableStateOf(Color.Yellow)
-    }
-    Box(modifier = modifier
-        .background(color.value)
-        .clickable {
-            color.value = Color(
+fun ColorBox(modifier: Modifier = Modifier, switcher: Int, updateColor: (Color) -> Unit) {
+
+    var mod = modifier
+    if (switcher == 1) {
+        mod = modifier.background(
+            Color(
                 Random.nextFloat(),
                 Random.nextFloat(),
                 Random.nextFloat(),
                 1f
             )
+        )
+    }
+
+    Box(modifier = mod
+        .clickable {
+            updateColor(
+                Color(
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    Random.nextFloat(),
+                    1f
+                )
+            )
         }
     )
+    {
+        Text(text = switcher.toString())
+    }
 }
 
 
