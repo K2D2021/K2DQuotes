8552820b3864bc63eed30c1aff28783d0f1d0969
K2D2021 <zxtreme@yandex.ru>
2023-03-02 01:45:44 +0300

    Learn how show snackbar, use TextField and run with CoroutineScope

--- app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
+++ app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
@@ -1,92 +1,61 @@
 package ru.k2d.k2dquotes
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import kotlin.properties.Delegates
-import kotlin.random.Random
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    var switcher by Delegates.notNull<Int>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        switcher = 0
-
         setContent {
-            Column(Modifier.fillMaxSize()) {
-                val color = remember {
-                    mutableStateOf(Color.Yellow)
-                }
-                ColorBox(
-                    Modifier
-                        .weight(1f)
-                        .fillMaxSize()
-                        .background(color.value),
-                    switcher
-                )
-                {
-                    color.value = it
-                    changeSwitcher()
-                }
-                Box(
+            val scaffoldState = rememberScaffoldState()
+            var textFieldState by remember {
+                mutableStateOf("")
+            }
+            val scope = rememberCoroutineScope()
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                scaffoldState = scaffoldState
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
                     modifier = Modifier
-                        .background(color.value)
-                        .weight(1f)
                         .fillMaxSize()
-                )
+                        .padding(horizontal = 30.dp)
+                ) {
+                    TextField(
+                        value = textFieldState,
+                        label = {
+                            Text("Enter your name")
+                        },
+                        onValueChange = {
+                            textFieldState = it
+                        },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(onClick = {
+                        scope.launch {
+                            scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
+                        }
+                    }) {
+                        Text("Please greet me")
+                    }
+                }
             }
         }
     }
-
-    private fun changeSwitcher() {
-        switcher = if (switcher == 0) 1 else 0
-    }
 }
 
-@SuppressLint("UnrememberedMutableState")
-@Composable
-fun ColorBox(modifier: Modifier = Modifier, switcher: Int, updateColor: (Color) -> Unit) {
-
-    var mod = modifier
-    if (switcher == 1) {
-        mod = modifier.background(
-            Color(
-                Random.nextFloat(),
-                Random.nextFloat(),
-                Random.nextFloat(),
-                1f
-            )
-        )
-    }
-
-    Box(modifier = mod
-        .clickable {
-            updateColor(
-                Color(
-                    Random.nextFloat(),
-                    Random.nextFloat(),
-                    Random.nextFloat(),
-                    1f
-                )
-            )
-        }
-    )
-    {
-        Text(text = switcher.toString())
-    }
-}
 
 
