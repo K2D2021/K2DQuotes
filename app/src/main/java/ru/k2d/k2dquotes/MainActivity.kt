8e1b74e2dc922c850928a24350f935bdc31fcebf
K2D2021 <zxtreme@yandex.ru>
2023-03-06 17:01:20 +0300

    Learn how to create very incredible animation by infinite.

--- app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
+++ app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.k2d.k2dquotes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -23,17 +24,23 @@ class MainActivity : ComponentActivity() {
             var sizeState by remember { mutableStateOf(200.dp) }
             val size by animateDpAsState(
                 targetValue = sizeState,
-                keyframes {
-                    durationMillis = 5000
-                    sizeState at 0 with LinearEasing
-                    sizeState * 1.5f at 1000 with FastOutLinearInEasing
-                    sizeState * 2f at 5000
-                }
+                tween(
+                    durationMillis = 1000
+                )
+            )
+            val infiniteTransition = rememberInfiniteTransition()
+            val color by infiniteTransition.animateColor(
+                initialValue = Color.Red,
+                targetValue = Color.Green,
+                animationSpec = infiniteRepeatable(
+                    tween(durationMillis = 2000),
+                    repeatMode = RepeatMode.Reverse
+                )
             )
             Box(
                 modifier = Modifier
                     .size(size)
-                    .background(Color.Red),
+                    .background(color),
                 contentAlignment = Alignment.Center
             ) {
                 Button(onClick = {
