1563617f6313ece303280f4daa42dfe7329af8a7
K2D2021 <zxtreme@yandex.ru>
2023-03-06 16:56:57 +0300

    Learn how to create very nice animation (keyframes)

--- app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
+++ app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
@@ -23,9 +23,12 @@ class MainActivity : ComponentActivity() {
             var sizeState by remember { mutableStateOf(200.dp) }
             val size by animateDpAsState(
                 targetValue = sizeState,
-                spring(
-                    Spring.DampingRatioHighBouncy
-                )
+                keyframes {
+                    durationMillis = 5000
+                    sizeState at 0 with LinearEasing
+                    sizeState * 1.5f at 1000 with FastOutLinearInEasing
+                    sizeState * 2f at 5000
+                }
             )
             Box(
                 modifier = Modifier
