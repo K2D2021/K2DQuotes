ccb88e955e04cd316405b445d4ba9eba6be00b78
K2D2021 <zxtreme@yandex.ru>
2023-03-12 23:57:10 +0300

    Add ic_baseline_search_24.xml

--- app/src/main/java/ru/k2d/k2dquotes/ui/HomeScreen.kt
+++ app/src/main/java/ru/k2d/k2dquotes/ui/HomeScreen.kt
@@ -1,2 +1,55 @@
 package ru.k2d.k2dquotes.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .background(Color.Blue)
+            .fillMaxSize()
+    )
+    {
+        Column() {
+
+        }
+    }
+}
+
+@Composable
+fun GreetingSection(
+    name: String = "K2D"
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Good morning, $name",
+                style = MaterialTheme.typography.h2
+            )
+            Text(
+                text = "We wish you have a good day!",
+                style = MaterialTheme.typography.body1
+            )
+        }
+        Icon(painter = painterResource(id = ru.k2d.k2dquotes.R.drawable.ic_baseline_search_24), contentDescription = "")
+    }
+
+}
