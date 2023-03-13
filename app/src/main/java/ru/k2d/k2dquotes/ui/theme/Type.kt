ccc2829eb6a97a3e7b67efa5ebed20749aae082a
K2D2021 <zxtreme@yandex.ru>
2023-03-13 23:17:39 +0300

    Tune text parameters

--- app/src/main/java/ru/k2d/k2dquotes/ui/theme/Type.kt
+++ app/src/main/java/ru/k2d/k2dquotes/ui/theme/Type.kt
@@ -2,27 +2,28 @@ package ru.k2d.k2dquotes.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        color = AquaBlue,
+        //fontFamily = gothicA1,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
         fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+    h1 = TextStyle(
+        color = TextWhite,
+        //fontFamily = gothicA1,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp
+    ),
+    h2 = TextStyle(
+        color = TextWhite,
+        //fontFamily = gothicA1,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
     )
-    */
+
 )
