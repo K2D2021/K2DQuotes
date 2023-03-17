af3d9c72e826af1ffbe725dd968ee4553dd83d07
K2D2021 <zxtreme@yandex.ru>
2023-03-17 22:53:53 +0300

    Clear

--- app/src/main/java/ru/k2d/k2dquotes/BottomMenuContent.kt
+++
@@ -1,8 +0,0 @@
-package ru.k2d.k2dquotes
-
-import androidx.annotation.DrawableRes
-
-data class BottomMenuContent(
-    val title: String,
-    @DrawableRes val iconId: Int
-)
--- app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
+++ app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
@@ -4,17 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.ui.ExperimentalComposeUiApi
-import ru.k2d.k2dquotes.ui.HomeScreen
-import ru.k2d.k2dquotes.ui.theme.K2DQuotesTheme
 
 @ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            K2DQuotesTheme {
-                HomeScreen()
-            }
+
         }
     }
 }
