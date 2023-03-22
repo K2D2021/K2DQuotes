0f84958fa64c22bcd8c8b860403000cb7e361aec
K2D2021 <zxtreme@yandex.ru>
2023-03-22 21:25:24 +0300

    Repair project after Studio updated

--- app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
+++ app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
@@ -3,7 +3,11 @@ package ru.k2d.k2dquotes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import ru.k2d.k2dquotes.ui.theme.K2DQuotesTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,9 +15,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             K2DQuotesTheme {
-                Text(text = "Hello")
+               Test()
             }
         }
     }
 }
 
+@Composable
+fun Test() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(text = "Hello")
+    }
+
+}
