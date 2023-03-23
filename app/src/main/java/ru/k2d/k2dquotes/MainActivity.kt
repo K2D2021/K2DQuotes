202fb0b2e8ffd71d7b7fe70823044870af3e0e7c
K2D2021 <zxtreme@yandex.ru>
2023-03-23 22:34:47 +0300

    Clean

--- app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
+++ app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
@@ -3,11 +3,6 @@ package ru.k2d.k2dquotes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import ru.k2d.k2dquotes.ui.theme.K2DQuotesTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,18 +10,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             K2DQuotesTheme {
-               Test()
+
             }
         }
     }
-}
-
-@Composable
-fun Test() {
-    Box(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(text = "Hello")
-    }
-
 }
