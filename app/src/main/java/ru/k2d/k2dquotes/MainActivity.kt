0537dd3ab03228401e9514655773ec9c1144fd5d
K2D2021 <zxtreme@yandex.ru>
2023-03-27 18:34:14 +0300

    Dagger2 dependencies added

--- app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
+++ app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
@@ -3,13 +3,28 @@ package ru.k2d.k2dquotes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Test()
         }
     }
 }
 
+@Composable
+fun Test() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Hello Dagger2")
+    }
+}
