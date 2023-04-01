aa6a6e315ba65408de2a4222238ea62de596ef89
K2D2021 <zxtreme@yandex.ru>
2023-04-01 23:30:35 +0300

    Initial commit

--- app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
+++ app/src/main/java/ru/k2d/k2dquotes/MainActivity.kt
@@ -3,12 +3,20 @@ package ru.k2d.k2dquotes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            Box(
+               modifier = Modifier.fillMaxSize() 
+            ){
+                Text(text = "It will be the best quotes app")
+            }
         }
     }
 }
